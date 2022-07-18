301a2304d6127f443478893ba97738ef0a21056d
zeeshanmehmood59640 <107610312+zeeshanmehmood59640@users.noreply.github.com>
2022-07-18 03:35:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+"using strict";
+
+const buttons = document.querySelectorAll(".drum");
+
+const lenght = buttons.length;
+// const audio = new Audio("sounds/tom-1.mp3");
+// document.querySelector(".drum").addEventListener("click", function () {
+//   audio.play();
+// });
+for (let i = 0; i < lenght; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    const buttonText = this.innerHTML;
+    makeSound(buttonText);
+    addAnimation(buttonText);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  addAnimation(event.key);
+});
+function makeSound(event) {
+  switch (event) {
+    case "w":
+      const audioW = new Audio("sounds/tom-1.mp3");
+      audioW.play();
+      break;
+    case "a":
+      const audioA = new Audio("sounds/tom-2.mp3");
+      audioA.play();
+      break;
+    case "s":
+      const audioS = new Audio("sounds/tom-3.mp3");
+      audioS.play();
+      break;
+    case "d":
+      const audioD = new Audio("sounds/tom-4.mp3");
+      audioD.play();
+      break;
+    case "j":
+      const audioJ = new Audio("sounds/crash.mp3");
+      audioJ.play();
+      break;
+    case "k":
+      const audioK = new Audio("sounds/kick-bass.mp3");
+      audioK.play();
+      break;
+    case "l":
+      const audioL = new Audio("sounds/snare.mp3");
+      audioL.play();
+      break;
+    default:
+  }
+}
+function addAnimation(key) {
+  const btn = document.querySelector("." + key);
+  btn.classList.add("pressed");
+  setTimeout(function () {
+    btn.classList.remove("pressed");
+  }, 100);
+}
