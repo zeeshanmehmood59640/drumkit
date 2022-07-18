c4098abbb59ee9df82787276b30e0d406c45e49f
zeeshanmehmood59640 <107610312+zeeshanmehmood59640@users.noreply.github.com>
2022-07-18 03:38:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const buttons = document.querySelectorAll(".drum");
 
 const lenght = buttons.length;
-// const audio = new Audio("sounds/tom-1.mp3");
+// const audio = new Audio("tom-1.mp3");
 // document.querySelector(".drum").addEventListener("click", function () {
 //   audio.play();
 // });
@@ -22,31 +22,31 @@ document.addEventListener("keydown", function (event) {
 function makeSound(event) {
   switch (event) {
     case "w":
-      const audioW = new Audio("sounds/tom-1.mp3");
+      const audioW = new Audio("tom-1.mp3");
       audioW.play();
       break;
     case "a":
-      const audioA = new Audio("sounds/tom-2.mp3");
+      const audioA = new Audio("tom-2.mp3");
       audioA.play();
       break;
     case "s":
-      const audioS = new Audio("sounds/tom-3.mp3");
+      const audioS = new Audio("tom-3.mp3");
       audioS.play();
       break;
     case "d":
-      const audioD = new Audio("sounds/tom-4.mp3");
+      const audioD = new Audio("tom-4.mp3");
       audioD.play();
       break;
     case "j":
-      const audioJ = new Audio("sounds/crash.mp3");
+      const audioJ = new Audio("crash.mp3");
       audioJ.play();
       break;
     case "k":
-      const audioK = new Audio("sounds/kick-bass.mp3");
+      const audioK = new Audio("kick-bass.mp3");
       audioK.play();
       break;
     case "l":
-      const audioL = new Audio("sounds/snare.mp3");
+      const audioL = new Audio("snare.mp3");
       audioL.play();
       break;
     default:
